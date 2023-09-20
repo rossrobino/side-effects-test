d0d3c9dbde21d465618aabacb0609285ba5ada49
rossrobino <rbrobino@gmail.com>
2023-09-20 09:19:55 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Breakpoint } from "drab";
+</script>
+
+<!-- No CSS in this component -->
+<Breakpoint />
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
+	 to read the documentation
+</p>
